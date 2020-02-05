d31bad409faaea93e6d2abfc9feae25d2a91eac5
Jane Kim <janek@mixmax.com>
2020-02-05 14:05:41 -0500

    feat: add sign functionality

--- calculator.js
+++ calculator.js
@@ -1,18 +1,20 @@
 function calculate(operator, a, b) {
   switch (operator) {
-    case "+":
+    case '+':
       return a + b;
-    case "-":
+    case '-':
       return a - b;
-    case "^":
+    case '^':
       return Math.pow(a, b);
-    case "*":
+    case '*':
       return a * b;
-    case "/":
+    case '/':
       return a / b;
-    case "√":
-    case "sqrt":
+    case '√':
+    case 'sqrt':
       return Math.sqrt(a);
+    case 'sign':
+      return Math.sign(a);
     default:
       break;
   }
