43401eb31193fb99b6525b18704f52e556d3962c
Eli Skeggs <eli@mixmax.com>
2020-02-05 11:02:48 -0800

    chore: tmp

--- calculator.js
+++ calculator.js
@@ -1,3 +1,21 @@
-function calculate(a, b, operator) {}
+function calculate(operator, a, b) {
+  switch (operator) {
+    case "+":
+      return a + b;
+    case "-":
+      return a - b;
+    case "^":
+      return Math.pow(a, b);
+    case "*":
+      return a * b;
+    case "/":
+      return a / b;
+    case "√":
+    case "sqrt":
+      return Math.sqrt(a);
+    default:
+      break;
+  }
+}
 
 module.exports = calculate;
--- calculator.test.js
+++ calculator.test.js
@@ -1,5 +1,33 @@
 const calculate = require("./calculator");
 
 test("test addition", function() {
-  expect(calculate(1, 1, "+")).toEqual(2);
+  expect(calculate("+", 1, 1)).toEqual(2);
+  expect(calculate("+", "1", "1")).toEqual("11");
+});
+
+test("test subtraction", function() {
+  expect(calculate("-", 3, 2)).toEqual(1);
+  expect(calculate("-", 2, -3)).toBe(5);
+});
+
+test("test power", function() {
+  expect(calculate("^", 2, 3)).toEqual(8);
+  expect(calculate("^", -2, -3)).toBe(-0.125);
+});
+
+test("test multiply", () => {
+  expect(calculate("*", 2, 4)).toBe(8);
+  expect(calculate("*", -4, 5)).toBe(-20);
+});
+
+test("test divide", () => {
+  expect(calculate("/", Infinity, Infinity)).toBe(NaN);
+  expect(calculate("/", 0, 0)).toBe(NaN);
+  expect(calculate("/", 1, 0)).toBe(Infinity);
+  expect(calculate("/", -1, 0)).toBe(-Infinity);
+});
+
+test("test sqrt", () => {
+  expect(calculate("√", 9)).toBe(3);
+  expect(calculate("√", -1)).toEqual(NaN);
 });
