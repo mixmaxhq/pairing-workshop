8b6b663bf8397502540188c4e18311af2bd83e0c
Jane Kim <janek@mixmax.com>
2020-02-05 14:08:06 -0500

    test: recirocal

--- calculator.test.js
+++ calculator.test.js
@@ -1,37 +1,42 @@
-const calculate = require("./calculator");
+const calculate = require('./calculator');
 
-test("test addition", function() {
-  expect(calculate("+", 1, 1)).toEqual(2);
-  expect(calculate("+", "1", "1")).toEqual("11");
+test('test addition', function() {
+  expect(calculate('+', 1, 1)).toEqual(2);
+  expect(calculate('+', '1', '1')).toEqual('11');
 });
 
-test("test subtraction", function() {
-  expect(calculate("-", 3, 2)).toEqual(1);
-  expect(calculate("-", 2, -3)).toBe(5);
+test('test subtraction', function() {
+  expect(calculate('-', 3, 2)).toEqual(1);
+  expect(calculate('-', 2, -3)).toBe(5);
 });
 
-test("test power", function() {
-  expect(calculate("^", 2, 3)).toEqual(8);
-  expect(calculate("^", -2, -3)).toBe(-0.125);
+test('test power', function() {
+  expect(calculate('^', 2, 3)).toEqual(8);
+  expect(calculate('^', -2, -3)).toBe(-0.125);
 });
 
-test("test multiply", () => {
-  expect(calculate("*", 2, 4)).toBe(8);
-  expect(calculate("*", -4, 5)).toBe(-20);
+test('test multiply', () => {
+  expect(calculate('*', 2, 4)).toBe(8);
+  expect(calculate('*', -4, 5)).toBe(-20);
 });
 
-test("test divide", () => {
-  expect(calculate("/", Infinity, Infinity)).toBe(NaN);
-  expect(calculate("/", 0, 0)).toBe(NaN);
-  expect(calculate("/", 1, 0)).toBe(Infinity);
-  expect(calculate("/", -1, 0)).toBe(-Infinity);
+test('test divide', () => {
+  expect(calculate('/', Infinity, Infinity)).toBe(NaN);
+  expect(calculate('/', 0, 0)).toBe(NaN);
+  expect(calculate('/', 1, 0)).toBe(Infinity);
+  expect(calculate('/', -1, 0)).toBe(-Infinity);
 });
 
-test("test sqrt", () => {
-  expect(calculate("√", 9)).toBe(3);
-  expect(calculate("√", -1)).toEqual(NaN);
+test('test sqrt', () => {
+  expect(calculate('√', 9)).toBe(3);
+  expect(calculate('√', -1)).toEqual(NaN);
 });
 
-test("test sign", () => {
-  expect(calculate("sign", 5)).toBe(1);
+test('test sign', () => {
+  expect(calculate('sign', 5)).toBe(1);
+  expect(calculate('sign', -1)).toBe(-1);
+});
+
+test('test reciprocal', () => {
+  expect(calculate('reciprocal', 5)).toBe(0.2);
 });
